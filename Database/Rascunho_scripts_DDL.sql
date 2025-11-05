5dda26a65187b65a66b43e55abee0a98ad3257d8
Gustavo Lima <82414372+tavinlima@users.noreply.github.com>
2025-11-05 19:29:58 -0300

    feat: add DDL, DML e DQL para cada entidade

--- Database/Rascunho_scripts_DDL.sql
+++ Database/Rascunho_scripts_DDL.sql
@@ -10,25 +10,26 @@ CREATE TABLE tb_usuarios (
     senha VARCHAR(255) NOT NULL,
     status VARCHAR(50) NOT NULL,
     data_nascimento DATE NOT NULL
-) 
+);
 
 -- Disciplinas
 CREATE TABLE tb_disciplinas (
     id_disciplina INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     id_professor INT NOT NULL,
-    carga_horaria TIME NOT NULL CHECK (carga_horaria >= 0),
+    carga_horaria DECIMAL(5,2) NOT NULL CHECK (carga_horaria >= 0),
+    numero_aulas INT NOT NULL,
     periodo VARCHAR(10) NOT NULL,
     CONSTRAINT fk_disciplinas_professor FOREIGN KEY (id_professor)
         REFERENCES tb_usuarios (id_usuario)
         ON DELETE RESTRICT
         ON UPDATE CASCADE
-)
+);
 
 -- Tabela: Matriculas
 CREATE TABLE tb_matriculas (
     id_matricula INT AUTO_INCREMENT PRIMARY KEY,
-    id_aluno INT NOT NULL,
+    id_aluno INT NOT NULL, 
     id_disciplina INT NOT NULL,
     data_matricula DATE NOT NULL,
     status VARCHAR(50) NOT NULL,
@@ -40,7 +41,7 @@ CREATE TABLE tb_matriculas (
         REFERENCES tb_disciplinas (id_disciplina)
         ON DELETE CASCADE
         ON UPDATE CASCADE
-)
+);
 
 -- Tabela: Notas
 CREATE TABLE tb_notas (
@@ -49,8 +50,41 @@ CREATE TABLE tb_notas (
     tipo_avaliacao VARCHAR(50) NOT NULL,
     nota DECIMAL(4,2) NOT NULL CHECK (nota >= 0 AND nota <= 10),
     data_lancamento TIMESTAMP NOT NULL,
+    peso DECIMAL(4,2) NOT NULL DEFAULT 1.0,
     CONSTRAINT fk_notas_matricula FOREIGN KEY (id_matricula)
         REFERENCES tb_matriculas (id_matricula)
         ON DELETE CASCADE
         ON UPDATE CASCADE
-)
+);
+
+INSERT into tb_usuarios (perfil,nome,email,senha,status,data_nascimento)
+VALUES
+('Professor', 'Roberto','roberto@email.com','senha123','matriculado','2000-01-01'),
+('Aluno', 'Felipe','felipe@email.com','senha123','formado','2005-06-01'),
+('Aluno', 'Luís','luis@email.com','senha123','trancado','1995-01-12'),
+('Coordenador', 'João','joao@email.com','senha123','ativo','1998-12-01'),
+('Professor', 'Claudia','claudia@email.com','senha123','ativo','2000-01-01');
+
+INSERT INTO tb_disciplinas(nome, id_professor, carga_horaria, numero_aulas, periodo)
+VALUES
+('Lógica de Programação',1,40.5,19,2025.1),
+('Desenvolvimento Web I',2,40.5,19,2025.2),
+('Banco de dados', 2, 40.5, 19, 2025.2);
+
+INSERT INTO tb_matriculas(id_aluno, id_disciplina, data_matricula, status)
+VALUES
+(2,1,'2025-01-02','trancado'),
+(3,2,'2025-07-02','matriculado'),
+(2,3,'2025-07-03','aprovado');
+
+INSERT INTO tb_notas(id_matricula, tipo_avaliacao, nota, data_lancamento)
+VALUES
+(1,'Prova',9.5,'2025-09-09'),
+(1,'Apresentação',10,'2025-09-09'),
+(1,'Prova',6,'2025-09-09'),
+(2,'Prova',8.5,'2025-10-09');
+
+SELECT * FROM tb_usuarios;
+SELECT * FROM tb_disciplinas;
+SELECT * FROM tb_matriculas;
+SELECT * FROM tb_notas;
